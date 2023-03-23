79f9708ac532c848a644216e323723cda177672c
Tresina Fernando <126407464+TresinaFernandoCSE@users.noreply.github.com>
2023-03-23 22:04:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var myName="Tresina";
+var collegename = prompt("What is your collegename?");
+alert("Hii I'm "+myName+ "Welcome to "+ collegename);
+
+//Arithmetic Operations
+
+prompt("Arithmetic Operations");
+var addition = 2+3;
+var subtraction = 5-3;
+var multiplication = 6*9;
+var quotitent = 5/2;
+var remainder = 5%2;
+
+//Increment and decrement
+
+var inc = 5;
+inc++;
+var dec = 6;
+dec--;
+
+//Dog age to human age calculator
+
+var dogage = 5;
+var humanage = (dogage-2)*4+21;
